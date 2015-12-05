3ea1f3480060ddb75b9435ca7420e5b5ca8415e9
Kocsen <kocsenc@gmail.com>
2015-12-04 19:56:29 -0500

    Implement add new quote feature.

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@
  * # MainCtrl
  * Controller of the hayboys app
  */
-var app = angular.module('hayboys', ['ngResource']);
+var app = angular.module('hayboys', ['ngResource', 'ngAnimate']);
 
 
 angular
@@ -15,16 +15,67 @@ angular
   .controller('main', MainCtrl);
 
 function MainCtrl($resource, $scope) {
-  var url = 'http://couch.kocsen.com/hayboys/_design/default/_view/default';
-  var QuotesResource = $resource(url);
+  var url = 'http://couch.kocsen.com/hayboys';
+  var QuotesResource = $resource(url + '/_design/default/_view/default');
+  var Quote = $resource(url);
   var vm = $scope;
 
-  vm.loading = true;
-  QuotesResource.get({descending: true}, function (data) {
-    vm.loading = false;
-    vm.quotes = data.rows;
-  }, function (err) {
-    alert(err);
-    vm.loading = false;
-  });
+  fetchQuotes();
+  function fetchQuotes() {
+    vm.loading = true;
+    QuotesResource.get({descending: true}, function (data) {
+      vm.loading = false;
+      vm.quotes = data.rows;
+    }, function (err) {
+      alert(err);
+      vm.loading = false;
+    });
+  }
+
+  vm.newQuote = {
+    form: {
+      //quote: '',
+      //author: '',
+      kk: 15
+    },
+    showError: false,
+    error: '',
+    show: false
+  };
+
+  vm.create = function () {
+    if (!vm.newQuote.form.quote) {
+      showError('Please fill out the quote!');
+      return
+    }
+    if (!vm.newQuote.form.author) {
+      showError('Who said that? Please fill out an author!');
+      return
+    }
+    if (!vm.newQuote.form.kk) {
+      showError('Please put only a number greater than 15 in 2K!');
+      return
+    }
+
+    var quote = new Quote({
+      quote: vm.newQuote.form.quote,
+      author: vm.newQuote.form.author,
+      created_at: new Date()
+    });
+    quote['2k'] = vm.newQuote.form.kk;
+
+    console.log(quote);
+    quote.$save().then(
+      function () {
+        fetchQuotes();
+        vm.newQuote.show = false;
+        vm.newQuote.showError = false;
+      }
+    )
+  };
+
+  function showError(message) {
+    vm.newQuote.error = message;
+    vm.newQuote.showError = true;
+  }
 }
